ba86c71879cac06aaa3acbefc1853dedd89cea03
Jakub Najman <jakub@najman.cz>
2023-07-24 15:50:27 +0200

    adjust grid button working

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-let dimensions = 16;
-for (let i = 0; i < dimensions; i++){
-    let row = document.createElement('div');
-    for (let k = 0; k < dimensions; k++){
-        let elem = document.createElement('div');
-        elem.style.width = "50px";
-        elem.style.height = "50px";
-        elem.classList.add("square");
-        row.appendChild(elem);
-        row.style.display = "flex";
+let gridSectionWidth = 960;
+function createGrid(dimensions) {
+    let gridWrapper = document.createElement('div')
+    gridWrapper.classList.add("gridWrapper");
+    for (let i = 0; i < dimensions; i++){
+        let row = document.createElement('div');
+        for (let k = 0; k < dimensions; k++){
+            let elem = document.createElement('div');
+            elem.style.width = 960/dimensions.toString() + "px";
+            elem.style.height = 960/dimensions.toString() + "px";
+            elem.classList.add("square");
+            row.appendChild(elem);
+            row.classList.add("grid-row");
+        }
+        gridWrapper.appendChild(row);
     }
-    document.body.appendChild(row);
+    return gridWrapper;
 }
+
+function displayGrid(grid){
+    let leftSection = document.querySelector('div[class=left]');
+    leftSection.width = gridSectionWidth;
+    leftSection.innerHTML = grid.innerHTML;
+}
+
+//make 16 by 16 for the beginning
+displayGrid(createGrid(16));
+
+
+let adjustButton = document.getElementById("adjustBtn");
+adjustButton.addEventListener("click", () => {
+    let newDimensions = prompt("Please enter new grid dimensions!");
+    if (newDimensions != null){
+        if (newDimensions > 100 || newDimensions <= 0) {
+            newDimensions = prompt("please enter dimensions between 1 - 100!");
+        }
+        displayGrid(createGrid(newDimensions));
+    }
+})
